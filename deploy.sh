7848fa660bee005c7daeaa88e2facff6fe39f5f6
Roman Pushkin <roman.pushkin@gmail.com>
2020-02-09 17:51:44 -0800

    Save

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git reset ./db/init.sql &&
+git checkout -- ./db/init.sql &&\
+cat ./migration/seed.sql >> ./db/init.sql &&\
+rm tmp.zip ; \
+zip -r tmp.zip * -x ./libretaxi -x ./migration/* -x ./.git/* && \
+echo "Uploading ..." && \
+scp tmp.zip ro@libretaxi.bot:/home/ro/go/src/libretaxi2/
+
