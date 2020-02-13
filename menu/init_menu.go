2bd4c6eabffa557c4ee648360d72726045aeb5d7
Roman Pushkin <roman.pushkin@gmail.com>
2020-02-12 18:14:46 -0800

    Save

--- menu/init_menu.go
+++ menu/init_menu.go
@@ -10,29 +10,37 @@ import (
 
 type InitMenuHandler struct {
 	context *context.Context
+	user *objects.User
 }
 
-func (handler *InitMenuHandler) postToPublicChannel(user *objects.User) {
+func (handler *InitMenuHandler) postToPublicChannel() {
+	// post to public channel once per user, so users can't spam it with /start command
+	if !handler.context.Repo.ShowCallout(handler.user.UserId, "public_channel_callout_when_initialized") {
+		return;
+	}
+
 	text := ""
 
-	if len(user.Username) == 0 {
-		userTextContact := fmt.Sprintf("[%s %s](tg://user?id=%d)", user.FirstName, user.LastName, user.UserId)
+	if len(handler.user.Username) == 0 {
+		userTextContact := fmt.Sprintf("[%s %s](tg://user?id=%d)", handler.user.FirstName, handler.user.LastName, handler.user.UserId)
 		text = fmt.Sprintf("%s has joined LibreTaxi", userTextContact)
 	} else {
-		text = fmt.Sprintf("@%s has joined LibreTaxi", user.Username)
+		text = fmt.Sprintf("@%s has joined LibreTaxi", handler.user.Username)
 	}
 
 	msg := tgbotapi.NewMessage(handler.context.Config.Public_Channel_Chat_Id, text)
-	if len(user.Username) == 0 {
+	if len(handler.user.Username) == 0 {
 		msg.ParseMode = "MarkdownV2"
 	}
 	handler.context.Send(msg)
+	handler.context.Repo.DismissCallout(handler.user.UserId, "public_channel_callout_when_initialized")
 }
 
 func (handler *InitMenuHandler) Handle(user *objects.User, context *context.Context, message *tgbotapi.Message) {
 	log.Println("Init menu")
 
 	handler.context = context
+	handler.user = user
 
 	// Send welcome message
 	msg := tgbotapi.NewMessage(user.UserId, user.Locale().Get("init_menu.welcome"))
@@ -41,7 +49,7 @@ func (handler *InitMenuHandler) Handle(user *objects.User, context *context.Cont
 	user.MenuId = objects.Menu_AskLocation
 	context.Repo.SaveUser(user)
 
-	handler.postToPublicChannel(user);
+	handler.postToPublicChannel();
 }
 
 func NewInitMenu() *InitMenuHandler {
